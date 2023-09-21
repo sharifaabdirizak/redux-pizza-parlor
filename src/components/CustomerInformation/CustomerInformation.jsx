4101274ddf7c3bd4910a5e392ad35c1bfb79d841
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-09-21 11:42:17 -0500

    created my handleNameChange, handleAddressChange, handleCityChange, handleZipChange set up pickand dispatch

--- src/components/CustomerInformation/CustomerInformation.jsx
+++ src/components/CustomerInformation/CustomerInformation.jsx
@@ -3,6 +3,50 @@ import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 function customInformation() {
-    const [customerName, setCustomerName] = useState('')
-    const [Address, setAddress]
+    const [customerName, setCustomerName] = useState('');
+    const [Address, setAddress] = useState('');
+    const [City, setCity] = useState('');
+    const [zipcode, setZipCode] = useState('')
 }
+
+const history = useHistory();
+const dispatch = useDispatch();
+
+const handleNameChange = (event) => {
+    setOrderToAdd({...orderToAdd,
+    customerName : event.target.value,
+})};
+
+
+const handleAddressChange = (event) => {
+    setOrderToAdd({...orderToAdd,
+    address : event.target.value,
+})};
+
+const handleCityChange = (event) => {
+    setOrderToAdd({...orderToAdd,
+    city : event.target.value,
+})};
+
+const handleZipcodeChange = (event) => {
+    setOrderToAdd({...orderToAdd,
+    zipcode : event.target.value,
+})};
+
+
+const handleDelivery = (event) => {
+    setOrderToAdd({...orderToAdd,
+    type : event.target.value,
+})};
+
+
+const setPickup = (event) => {
+    event.preventDefault()
+    console.log('orderToAdd')
+
+    dipatch ({
+        type: 'Set Customer',
+        payload: orderToAdd
+    }); history.push('/checkout')
+}
+    
