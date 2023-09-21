650be2d9bd2ad632e1cb9d66f93563e9475dca58
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-09-21 11:13:13 -0500

    created function called customerinformation

--- src/components/CustomerInformation/CustomerInformation.jsx
+++ src/components/CustomerInformation/CustomerInformation.jsx
@@ -1,3 +1,8 @@
 import { useState } from "react"
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
+
+function customInformation() {
+    const [customerName, setCustomerName] = useState('')
+    const [Address, setAddress]
+}
