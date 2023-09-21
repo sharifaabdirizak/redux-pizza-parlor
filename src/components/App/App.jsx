b8b1d7033cb9598e34b4139061502b4946f26fc9
JDNafz <nafziger10@gmail.com>
2023-09-21 11:07:13 -0500

    ready to merge

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,54 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+// App.js
+import "./App.css";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
-    </div>
+    <Router>
+      <div className="nav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/SelectPizza">Add Artist</Link>
+        </li>
+        <li>
+          <Link to="/CustomerInformation">All Artists</Link>
+        </li>
+        <li>
+          <Link to="/OrderCheckout">All Artists</Link>
+        </li>
+        <li>
+          {/* Remove this later */}
+          <Link to="/Admin">Admin</Link>
+        </li>
+      </div>
+      <Route exact path="/">
+        <Home />
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Prime Pizza</h1>
+          </header>
+
+          <img src="images/pizza_photo.png" />
+          <p>Pizza is great.</p>
+        </div>
+      </Route>
+
+      <Route exact path="/SelectPizza">
+        <SelectPizza />
+      </Route>
+
+      <Route exact path="/CustomerInformation">
+        <CustomerInformation />
+      </Route>
+      <Route exact path="/OrderCheckout">
+        <OrderCheckout />
+      </Route>
+      <Route exact path="/Admin">
+        <Admin />
+      </Route>
+    </Router>
   );
 }
 
